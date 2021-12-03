e2a18edf6d57f8b52c34f7b8a65f6cbbc56f3ef9
deimar <deimar.parn@khk.ee>
2021-12-03 09:25:47 +0000

    Change city wather data

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // Weather object
 const weather = new Weather('Tallinn')
 console.log(weather.getWeather())
+// UI object
+const ui = new UI()
+
+// events
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeCityWeather)
+
+function changeCityWeather(event){
+    const city = document.querySelector('#city-name').value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector('#city-name').value = ''
+    event.preventDefault()
+}
 
 function getWeather(){
      weather.getWeather()
     .then(data => {
         console.log(data)
+        ui.drawWeather(data)
     })
          .catch(error => console.log(error))
 }
